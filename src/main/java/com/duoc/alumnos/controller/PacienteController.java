513142494c4e041cf35464cd799f30eaf789af8d
PAAPCA02LC200 <PAAPCA02LC200@PAAPCA02LC200.duoc.acad>
2025-04-30 22:06:53 -0400

    Avances de programacion

---
+++ src/main/java/com/duoc/alumnos/controller/PacienteController.java
@@ -0,0 +1,15 @@
+package com.duoc.alumnos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PacienteController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Home pacientes";
+    }
+}
